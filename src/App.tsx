547369dc21ae423ba8053bb4a0807babaa9c47c3
Mrufaihi <malrufaihi@gmail.com>
2024-10-29 11:04:46 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="m-3">
+      <h1 className="text-3xl text-emerald-700 font-bold ">To Do List</h1>
+      <ul className="list">
+        <li className="list-item">task 1</li>
+        <li className="list-item">task 2</li>
+        <li className="list-item">task 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
